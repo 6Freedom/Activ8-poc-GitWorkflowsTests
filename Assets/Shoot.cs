efb02f48dba7d2a018ef13844ca708d2b3862a1f
Jacques <jacques.clery--salenave@u-psud.fr>
2020-10-30 12:10:57 +0100

    project done

---
+++ Assets/Shoot.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    [SerializeField] private GameObject sphere;
+    [SerializeField] private float shootForce = 2;
+    
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            GameObject shootedSphere = Instantiate(sphere, transform.position, transform.rotation);
+            
+            Vector3 force = new Vector3(transform.rotation.x, transform.rotation.y, transform.rotation.z);
+
+            force.Normalize();
+
+            force *= shootForce;
+            
+            shootedSphere.GetComponent<Rigidbody>().AddForce(force);
+        }
+    }
+}
